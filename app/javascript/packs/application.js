cfd900a4709a06f41c345298a0d5dfe1163dc235
Denis Naughton <mjolinor75@hotmail.com>
2021-03-05 19:56:56 -0500

    Added gem stimulus_reflex

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +18,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+StimulusReflex.initialize(application, { consumer, controller, isolate: true })
+StimulusReflex.debug = process.env.RAILS_ENV === 'development'
